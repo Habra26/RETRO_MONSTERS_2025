a3e46e8caf662ad5b00921dbae1f068d141e09e4
Habra26 <arnaud.habraken@outlook.com>
2025-06-11 13:06:15 +0200

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,9 @@
+<?php
+// PARAMS
+
+// Paramètres de connexion à la db
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'retro_monsters_2025');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
